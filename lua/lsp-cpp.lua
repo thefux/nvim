d15e3880118f49ea7dc308c029903883f39075ed
thefux <thefux@github.com>
2022-01-26 19:00:02 +0100

    update cpp lsp config

--- lua/lsp-cpp.lua
+++ lua/lsp-cpp.lua
@@ -1,7 +1,11 @@
 require'lspconfig'.ccls.setup {
   init_options = {
+    compilationDatabaseDirectory = "build";
+    index = {
+      threads = 1;
+    };
     cache = {
-      directory = ".ccls-cache";
+      directory = "$HOME/AppData/Local/Temp/ccls-cache";
     };
   }
 }
